b755bcf5d17d087c3098281d9b5b6f87f6da23e0
Krasimir Mladenov <kdmladenov@yahoo.com>
2023-05-05 14:53:24 +0300

    SQL swapped

--- config.ts
+++ config.ts
@@ -1,7 +1,21 @@
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 import { PoolConfig } from 'mariadb';
 
-const config = dotenv.config().parsed!;
+// const config = dotenv.config().parsed!;
+const config = {
+  PORT: '5555',
+  HOST: 'bkfyhkzemx2bdsdphqbq-mysql.services.clever-cloud.com',
+  DBPORT: '3306',
+  USER: 'uvmgjjeyvrxwdg6c',
+  PASSWORD: '1O5GHe7472OE381TnpO5',
+  DATABASE: 'bkfyhkzemx2bdsdphqbq',
+  PRIVATE_KEY: 'sekreten_chasten_klu4',
+  TOKEN_LIFETIME: '360000',
+  LIMIT: 3,
+  EMAIL_SERVICE: 'hotmail',
+  EMAIL_USER: 'smartgaragekd@outlook.com',
+  EMAIL_PASSWORD: 'BestSuperSekretenklu4'
+};
 
 export const DB_CONFIG: PoolConfig = {
   host: config.HOST,
@@ -10,6 +24,7 @@ export const DB_CONFIG: PoolConfig = {
   password: config.PASSWORD,
   database: config.DATABASE,
   connectionLimit: +config.LIMIT,
+  allowPublicKeyRetrieval: true,
 };
 
 export const {
@@ -20,9 +35,3 @@ export const {
   EMAIL_USER,
   EMAIL_PASSWORD,
 } = config;
-
-// export const {
-//   emailService: "hotmail",
-//   emailUser: "smartgaragekd@outlook.com",
-//   emailPassword: "Sekretenklu4",
-// };
